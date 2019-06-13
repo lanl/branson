34bb1221fe31436f6ad0ce7a44ffb735f8ce8ffc
Alex R. Long <along@lanl.gov>
2019-06-13 13:50:18 -0600

    + Don't use allreduce in plotting routines when in replicated modes

--- src/write_silo.h
+++ src/write_silo.h
@@ -30,7 +30,7 @@
 // writes the SILO file for visualization
 void write_silo(const Mesh &mesh, const double &arg_time, const uint32_t &step,
                 const double &r_transport_time, const double &r_mpi_time,
-                const int &rank, const int &n_rank) {
+                const int &rank, const int &n_rank, bool rep_flag = true) {
 
 #ifdef VIZ_LIBRARIES_FOUND
   using Constants::ELEMENT;
@@ -108,29 +108,32 @@ void write_silo(const Mesh &mesh, const double &arg_time, const uint32_t &step,
     grip_ID[silo_index] = cell.get_grip_ID();
   }
 
-  // reduce to get rank of each cell across all ranks
-  MPI_Allreduce(MPI_IN_PLACE, &region_data[0], n_xyz_cells, MPI_INT, MPI_SUM,
-                MPI_COMM_WORLD);
+  // don't reduce these quantities in replicated mode
+  if (!rep_flag) {
+    // reduce to get rank of each cell across all ranks
+    MPI_Allreduce(MPI_IN_PLACE, &region_data[0], n_xyz_cells, MPI_INT, MPI_SUM,
+                  MPI_COMM_WORLD);
 
-  // reduce to get T_e across all ranks
-  MPI_Allreduce(MPI_IN_PLACE, &T_e[0], n_xyz_cells, MPI_DOUBLE, MPI_SUM,
-                MPI_COMM_WORLD);
+    // reduce to get T_e across all ranks
+    MPI_Allreduce(MPI_IN_PLACE, &T_e[0], n_xyz_cells, MPI_DOUBLE, MPI_SUM,
+                  MPI_COMM_WORLD);
 
-  // reduce to get T_r across all ranks
-  MPI_Allreduce(MPI_IN_PLACE, &T_r[0], n_xyz_cells, MPI_DOUBLE, MPI_SUM,
-                MPI_COMM_WORLD);
+    // reduce to get T_r across all ranks
+    MPI_Allreduce(MPI_IN_PLACE, &T_r[0], n_xyz_cells, MPI_DOUBLE, MPI_SUM,
+                  MPI_COMM_WORLD);
 
-  // reduce to get transport runtime from all ranks
-  MPI_Allreduce(MPI_IN_PLACE, &transport_time[0], n_xyz_cells, MPI_DOUBLE,
-                MPI_SUM, MPI_COMM_WORLD);
+    // reduce to get transport runtime from all ranks
+    MPI_Allreduce(MPI_IN_PLACE, &transport_time[0], n_xyz_cells, MPI_DOUBLE,
+                  MPI_SUM, MPI_COMM_WORLD);
 
-  // reduce to get mpi time from all ranks
-  MPI_Allreduce(MPI_IN_PLACE, &mpi_time[0], n_xyz_cells, MPI_DOUBLE, MPI_SUM,
-                MPI_COMM_WORLD);
+    // reduce to get mpi time from all ranks
+    MPI_Allreduce(MPI_IN_PLACE, &mpi_time[0], n_xyz_cells, MPI_DOUBLE, MPI_SUM,
+                  MPI_COMM_WORLD);
 
-  // reduce to get grip_ID across all ranks
-  MPI_Allreduce(MPI_IN_PLACE, &grip_ID[0], n_xyz_cells, MPI_INT, MPI_SUM,
-                MPI_COMM_WORLD);
+    // reduce to get grip_ID across all ranks
+    MPI_Allreduce(MPI_IN_PLACE, &grip_ID[0], n_xyz_cells, MPI_INT, MPI_SUM,
+                  MPI_COMM_WORLD);
+  }
 
   // First rank writes the SILO file
   if (rank == 0) {
