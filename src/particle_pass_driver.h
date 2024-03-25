3015841ed218218fb1a505f050cb603a61328ec0
Galen M. Shipman <Galen Shipman>
2024-03-25 17:34:19 -0600

    add caliper to particle pass driver

--- src/particle_pass_driver.h
+++ src/particle_pass_driver.h
@@ -64,7 +64,9 @@ void imc_particle_pass_driver(Mesh &mesh, IMC_State &imc_state,
     mctr.reset_counters();
 
     //set opacity, Fleck factor, all energy to source
+    wrapped_cali_mark_begin("calculate photon energy");
     mesh.calculate_photon_energy(imc_state, n_user_photons);
+    wrapped_cali_mark_end("calculate photon energy");
 
     // all reduce to get total source energy to make correct number of
     // particles on each rank
@@ -72,31 +74,46 @@ void imc_particle_pass_driver(Mesh &mesh, IMC_State &imc_state,
     MPI_Allreduce(MPI_IN_PLACE, &global_source_energy, 1, MPI_DOUBLE, MPI_SUM,
                   MPI_COMM_WORLD);
 
+    wrapped_cali_mark_begin("calculate pre census energy");
     imc_state.set_pre_census_E(get_photon_list_E(census_photons));
+    wrapped_cali_mark_end("calculate pre census energy");
 
     // make gpu setup object, may want to source on GPU later so make it before sourcing here
     GPU_Setup gpu_setup(rank, n_ranks, imc_parameters.get_use_gpu_transporter_flag(), mesh.get_cells());
 
     // setup source
-    if (imc_state.get_step() == 1)
+    if (imc_state.get_step() == 1) {
+      wrapped_cali_mark_begin("make initial census photons");
       census_photons = make_initial_census_photons(imc_state.get_dt(), mesh, rank, seed, n_user_photons, global_source_energy);
+      wrapped_cali_mark_end("make initial census photons");
+    }
 
     imc_state.set_pre_census_E(get_photon_list_E(census_photons));
     MPI_Barrier(MPI_COMM_WORLD);
     // make emission and source photons
+    wrapped_cali_mark_begin("make emission and source photons");
     auto all_photons = make_photons(imc_state.get_dt(), mesh, rank, imc_state.get_step(), seed, n_user_photons, global_source_energy);
+    wrapped_cali_mark_end("make emission and source photons");
     // add the census photons
+    wrapped_cali_mark_begin("add the census photons");
     all_photons.insert(all_photons.end(), census_photons.begin(), census_photons.end());
+    wrapped_cali_mark_end("add the census photons");
 
+    wrapped_cali_mark_begin("set transported particles");
     imc_state.set_transported_particles(all_photons.size());
-
+    wrapped_cali_mark_end("set transported particles");
+    
     imc_state.print_memory_estimate(rank, n_ranks, mesh.get_n_local_cells(), all_photons.size());
 
     // add barrier here to make sure the transport timer starts at roughly the same time
     MPI_Barrier(MPI_COMM_WORLD);
+    wrapped_cali_mark_begin("transport photons");
     census_photons = particle_pass_transport(mesh, gpu_setup, imc_parameters, mpi_info, mpi_types, imc_state, mctr, abs_E, track_E, all_photons, imc_parameters.get_n_omp_threads());
+    wrapped_cali_mark_end("transport photons");
 
+    wrapped_cali_mark_begin("update temperature");
     mesh.update_temperature(abs_E, track_E, imc_state);
+    wrapped_cali_mark_end("update temperature");
 
     // update time for next step
     imc_state.print_conservation(imc_parameters.get_dd_mode());
@@ -118,7 +135,10 @@ void imc_particle_pass_driver(Mesh &mesh, IMC_State &imc_state,
     mem_record.read_meminfo(meminfo_string);
 #endif
 
+    wrapped_cali_mark_begin("update time for next step");
     imc_state.next_time_step();
+    wrapped_cali_mark_end("update time for next step");
+    
   }
 
 #ifdef USE_MEMORY_RECORD
